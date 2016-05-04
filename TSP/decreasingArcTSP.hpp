c4e3bc5256b68bdfa158cbe79d12d42745a94ffe
amilliet <amilliet@packers.pedago.ipb.fr>
2016-05-04 09:44:26 +0200

    starting decreasingArc

---
+++ TSP/decreasingArcTSP.hpp
@@ -0,0 +1,23 @@
+#ifndef _CHAINE_HPP_
+#define _CHAINE_HPP_
+
+class decreasingArc{
+
+  vector <int> Sommets;
+  int cpt = 1;
+  
+
+
+public:
+  ~decreasingArc();
+  decreasingArc();
+  void path(vector <int> Sommets);
+}
+
+
+
+
+
+
+
+#endif
