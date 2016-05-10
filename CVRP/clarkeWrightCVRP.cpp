067df8fdbe61da9d5ee778d60ef4cec319549e4d
amilliet <amilliet@babouin.pedago.ipb.fr>
2016-05-10 16:36:40 +0200

    Début algo

--- CVRP/clarkeWrightCVRP.cpp
+++ CVRP/clarkeWrightCVRP.cpp
@@ -2,5 +2,24 @@
 #include <vector>
 #include <algorithm>
 #include <unistd.h>
+#include <list>
 
 #include "clarkeWrightCVRP.hpp"
+#include "CVRP.hpp"
+#include "tour.hpp"
+
+clarkeWrightCVRP::clarkeWrightCVRP(){}
+
+clarkeWrightCVRP::~clarkeWrightCVRP(){}
+
+void clarkeWrightCVRP::clarkeWrightProcedure(){
+
+  list<struct tour> L; // list of tours, will be the result at the end of the procedure
+  for (int i = 0; i < NbNodes; i++}{
+    struct tour tmp_tour;
+    tmp_tour.id = i;
+    tmp_tour.clients_order.push_back(i + 'i');
+  }
+}
+
+int clarkeWrightCVRP::savingPerConcatenation(){}
--- CVRP/clarkeWrightCVRP.hpp
+++ CVRP/clarkeWrightCVRP.hpp
@@ -1,4 +1,27 @@
 #ifndef _CLARKE_WRIGHT_CVRP_HPP_
 #define _CLARKE_WRIGHT_CVRP_HPP_
 
+
+#include "CVRP.hpp"
+
+using namespace std;
+
+class clarkeWrightCVRP{
+  int Capacity;
+  int NbNodes;
+  int ** Distances;
+  int * Demands;
+
+
+
+
+public:
+  clarkeWrightCVRP(const TSPData&);
+  ~clarkeWrightCVRP();
+  void clarkeWrightProcedure(CVRPData);
+  int savingPerConcatenation();
+
+
+};
+
 #endif
