9814dceba8cf51872bccaa3c46319927582ec07e
amilliet <amilliet@sardane.pedago.ipb.fr>
2016-05-12 13:06:45 +0200

    ajout tour et list_tour

---
+++ CVRP/list_tour.hpp
@@ -0,0 +1,12 @@
+#ifndef _LIST_TOUR_HPP_
+#define _LIST_TOUR_HPP_
+
+using namespace std;
+
+struct list_tour{
+  list<struct tour> tour_list;
+  char* name;
+  int size;
+};
+
+#endif
---
+++ CVRP/tour.hpp
@@ -0,0 +1,13 @@
+#ifndef _TOUR_HPP_
+#define _TOUR_HPP_
+
+using namespace std;
+
+struct tour{
+  list<char> clients_order;
+  int id;
+  int length;
+  int used_capacity;
+};
+
+#endif
